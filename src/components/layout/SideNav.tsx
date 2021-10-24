6fba517fe0267cf25feef0e2ec3bbede91a3d79d
Jatin Kathuria <jtn.kathuria@gmail.com>
2021-10-25 01:17:05 +0200

    make responsive

--- src/components/layout/SideNav.tsx
+++ src/components/layout/SideNav.tsx
@@ -10,17 +10,17 @@ const SideNav = forwardRef<HTMLDivElement, HTMLProps<HTMLDivElement>>(
 
     return (
       <aside
-        className={`relative sidenav__container bg-primary ${
+        className={`sidenav__container bg-primary ${
           className ? className : ""
         }
-        ${collapsed ? "collapsed" : ""}
+        ${collapsed ? "collapsed" : ""} absolute md:relative z-10 h-screen
         `}
         ref={ref}
         {...restProps}
       >
         <div className="relative w-full h-full">
           <div
-            className="absolute right-0 z-10 w-8 h-8 cursor-pointer sidenav__switch top-20 bg-accent md:hidden"
+            className="absolute right-0 z-10 w-8 h-8 cursor-pointer sidenav__switch top-2 bg-accent md:hidden"
             style={{
               transform: " translateX(100%)",
             }}
